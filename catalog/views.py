3116f77a53e01c0117aa47540519d440a7de6e21
NguyenDungB <nguyen.thi.dungb@sun-asterisk.com>
2020-08-10 13:24:15 +0700

    Part 7: Sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
 
 def index(request):
     """View function for home page of site."""
@@ -16,13 +18,52 @@ def index(request):
     
     num_genres = Genre.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres' : num_genres,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    def get_queryset(self):
+        return Book.objects.all()[:5] # Get 5 books containing the title war
+  
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+        # try:
+        #     book = Book.objects.get(pk=primary_key)
+        # except Book.DoesNotExist:
+        #     raise Http404('Book does not exist')
+    
+        # return render(request, 'catalog/book_detail.html', context={'book': book})
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'book_detail.html', context={'book': book})
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+    # tmplate_name = 'author_list.html'
+    def get_queryset(self):
+        return Author.objects.all()[:5]
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    def author_detail_view(request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request,'author_detail.html',context={'author': author})       
+
