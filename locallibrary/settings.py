67ed1ba3090f1b96e94357e1c3fb66adf8d956b4
NguyenDungB <nguyen.thi.dungb@sun-asterisk.com>
2020-08-11 11:25:58 +0700

    Part 9: Working with forms

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
