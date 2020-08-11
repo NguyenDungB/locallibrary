4e5dc2b4ead0650db989adb6d84409b2111aec23
NguyenDungB <nguyen.thi.dungb@sun-asterisk.com>
2020-08-11 11:25:58 +0700

    Part 10: Testing a Django web application

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,7 @@
-# import datetime
+import datetime
 
 # from django import forms
-# from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 # class RenewBookForm(forms.Form):
